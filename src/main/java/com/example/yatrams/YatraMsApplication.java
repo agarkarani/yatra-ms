f9d0a54d4e42dfc4af4b3ad5aa0ccf4e23cf3dd4
ANIKET AGARKAR <aniketagarkar16@gmail.com>
2023-10-19 17:22:18 +0530

    coderefactor

---
+++ src/main/java/com/example/yatrams/YatraMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.yatrams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatraMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatraMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yatrams/YatraMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yatrams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatraMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
